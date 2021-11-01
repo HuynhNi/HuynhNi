fcb957ecf45fd05d8f6cd8377ea2ae9e0b503b20
anhanh111 <bachtran1p@gmail.com>
2021-11-01 15:29:44 +0700

    first commit

---
+++ src/basic/dev/MainApp.java
@@ -0,0 +1,50 @@
+package basic.dev;
+
+
+public class MainApp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	//Thông tin sinh viên
+		String fullName = "Phan Tien Huynh";
+		String id = "2160163";
+		String cccd = "045637173833";
+		String tl ="21cdth41";
+		String tr = "Hueic";
+		float dtb = 10f ;
+		
+		System.out.println("Tên: " + fullName );
+		System.out.println("Msv: "+ id);
+		System.out.println("cccd: "+ cccd);
+		System.out.println("Tên Lớp: " + tl );
+		System.out.println("Tên trường: " + tr );
+		System.out.println("Điểm trung bình: " + dtb );
+		
+		//Thông tin công nhân
+		String name = "Phan Tiến Huynh";
+		String đc = "Huế";
+		String sdt = "0989590165";
+		float hsl = 56f;
+		
+		System.out.println("Tên: " + name);
+		System.out.println("Địa chỉ: " + đc);
+		System.out.println("Số điện thoại: " + sdt);
+		System.out.println("Hệ số lượng: " + hsl);
+		
+		//Thông tin một chiếc xe car
+		String Name = "lambogini";
+		String hang = "Sh";
+		float gia = 50f;
+		String ms = "đỏ";
+		
+		System.out.println("Tên: " + Name);
+		System.out.println("Hãng: " + hang);
+		System.out.println("Giá: " + gia);
+		System.out.println("Màu sơn: " + ms);
+		
+		
+		
+		
+	}
+
+}
